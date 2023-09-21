981a7baced04972d13cc12f3ec52ff64953f5019
gradedSystem <homesiknessgerald@gmail.com>
2023-09-21 15:36:20 +0200

    [feat][l] Changing data packages updating the code - add/replace #2

--- scripts/process.py
+++ scripts/process.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import os
+import csv
+from io import StringIO
 import urllib.request
 import requests
+import pandas as pd
 from config import api_key
 from requests.exceptions import HTTPError
 
 # Option 1:
 data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                         '..', 'data')
+                                         '..', 'archive')
 source = 'http://ourairports.com/data/airports.csv'
-archive = os.path.join(data_folder, 'data.csv')
+archive = os.path.join(data_folder, 'source.csv')
 
 # Option 2:
 API_URL = "https://api.eia.gov/v2/natural-gas/pri/fut/data/"
 
+# Folder path of an archive
+data_csv_path = os.path.join('archive', 'source.csv')
+
+# Parameters of an API
 params = {
     'api_key': api_key,
     'frequency': 'daily',
@@ -32,7 +39,7 @@ def url_retrieve(url, destination):
             os.makedirs(destination)
         response = urllib.request.urlretrieve(url, 
                                               os.path.join(destination,
-                                                            'data.csv'))
+                                                            'source.csv'))
         print("Downloaded data successfully!")
     except Exception as err:
         print(f"Error occurred while downloading data: {err}")
@@ -59,9 +66,71 @@ def api_retrieve(url, destination):
     else:
         print("Success!")
 
+def clean_and_prepare(file):
+    """
+    1)
+        Read CSV and create separate .md file for comments
+    2)  
+        data files have no empty rows
+    3)
+        Using separator as "." instead of ",".
+    4)
+        Replace None values with "0" and drop rows with empty cells
+    5)
+        Unpivot the csv table by "id" variable.
+    """
+    # 1) Drop the comments and create separate .md file for comments
+    # Define the character used for comments (e.g., '#')
+    comment_character = '#'
+
+    # Initialize variables to store data and comments
+    data_lines = []
+    comment_lines = []
+
+    # Read the data file and separate data from comments
+    with open(file, 'r', encoding='utf-8') as file:
+        for line in file:
+            if line.startswith(comment_character):
+                comment_lines.append(line)
+            else:
+                data_lines.append(line)
+    
+    # Write the comments to a Markdown file
+    with open('comments.md', 'w') as comments_file:
+        comments_file.writelines("# Removed comments: \n")
+        comments_file.writelines(comment_lines)
+    
+    # Parse the data without comments as a CSV to create a DataFrame
+    data_string = ''.join(data_lines)
+    
+    df = pd.read_csv(StringIO(data_string));
+    
+    # 2) Remove empty rows
+    df = df.dropna(how='all')
+    
+    # 3) Use separator as "." instead of ","
+    for col in df.columns:
+        df[col] = df[col].replace({',': '.'}, regex=True)
+
+    # 4) Replace None values with "0"
+    df = df.fillna(0)  
+
+    # 5) Unpivot the data
+    col_df = list(df.columns.values)
+    df = pd.melt(df,id_vars = col_df[0], 
+                 value_vars = col_df[1:], ignore_index=False)
+    
+    # Save CSV into data folder
+    data_folder_path = 'data/'  # Specify the path to the 'data' folder
+    out_csv_path = data_folder_path + 'data.csv'
+    df.to_csv(out_csv_path, index=False)
+    
+
+
 def main():
     url_retrieve(source, data_folder)
     api_retrieve(API_URL, data_folder)
+    clean_and_prepare(data_csv_path)
 
 if __name__ == "__main__":
     main()
